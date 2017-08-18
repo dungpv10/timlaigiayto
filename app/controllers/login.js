2a575396af01f8caa5332a382e547b46db900459
Pham Dung <dungpv10@gmail.com>
2017-08-19 01:31:01 +0700

    Fix login

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -2,13 +2,17 @@
 let Auth = require('../libs/Auth');
 let LoginController = {
     index: (req, res) => {
-        res.render('auth/login');
+        if(req.session.userLogged !== null) res.redirect('/admin');
+        else res.render('auth/login');
     },
 
     postLogin: (req, res) => {
         Auth.login({email : req.body.email, password : req.body.password}, (result) => {
             if(result === false) res.redirect('/admin/login');
-            else req.session.userLogged = result || null;
+            else {
+                req.session.userLogged = result || null;
+                res.redirect('/admin');
+            }
         });
     }
 };
